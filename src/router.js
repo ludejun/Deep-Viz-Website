5cfaf90e64feaf7d1183839cd8d6c51c0f4b1834
hannah <hannah@hannahs-MacBook-Pro.local>
2017-11-30 11:08:32 +0800

    add before home page

--- src/router.js
+++ src/router.js
@@ -8,6 +8,7 @@ export default ({ history }) => {
     <Router history={history}>
       <Route path="/" component={routes.App}>
         <IndexRoute component={routes.Home} />
+        <Route path="front-page" components={routes.BeforeHome} />
         <Route path="main" components={routes.Main}>
           <IndexRoute component={routes.Introduction} />
           <Route path="introduction" title="关于Deep-Viz" component={routes.Introduction} />
@@ -86,9 +87,17 @@ export default ({ history }) => {
                 title="BaiduMapCrossCurve 百度地图迁徙流向图"
                 component={routes.BaiduMapCrossCurve}
               />
-              <Route path="AMapCluster" title="AMapCluster 高德地图点聚合" component={routes.AMapCluster} />
+              <Route
+                path="AMapCluster"
+                title="AMapCluster 高德地图点聚合"
+                component={routes.AMapCluster}
+              />
               <Route path="aMapIndoor" title="AMapIndoor 高德地图室内图" component={routes.AMapIndoor} />
-              <Route path="aMapDistrictCluster" title="AMapDistrictCluster 高德地图行政聚合图" component={routes.AMapDistrictCluster} />
+              <Route
+                path="aMapDistrictCluster"
+                title="AMapDistrictCluster 高德地图行政聚合图"
+                component={routes.AMapDistrictCluster}
+              />
             </Route>
             <Route path="pictogram" title="Pictogram 象形图组件">
               <Route path="image-bar" title="ImageBar 柱状象形图" component={routes.ImageBar} />
@@ -106,7 +115,11 @@ export default ({ history }) => {
             <Route path="canvas" title="Canvas 组件">
               <Route path="Gauge" title="Gauge 仪表盘" component={routes.Gauge} />
               <Route path="pillar-list" title="PillarList 立体簇状柱形图" component={routes.PillarList} />
-              <Route path="scatter-curve" title="ScatterCurveMap 多样迁徙地图" component={routes.ScatterCurve} />
+              <Route
+                path="scatter-curve"
+                title="ScatterCurveMap 多样迁徙地图"
+                component={routes.ScatterCurve}
+              />
               <Route
                 path="CircleAnimate"
                 title="CircleAnimate 气泡Canvas"
---
+++ src/routes/BeforeHome/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ScrollAnim, { Link as AnimeLink, Element, OverPack } from 'rc-scroll-anim';
+import QueueAnim from 'rc-queue-anim';
+import TweenOne from 'rc-tween-one';
+import './beforeHome.less';
+
+// ScrollAnim.scrollScreen.init();
+export default class BeforeHome extends React.Component {
+  render() {
+    return (
+      <div style={{ height: '100%' }}>
+        <div className="content-wrapper">
+          {/*
+          <div className="nav">
+            <AnimeLink className="nav-list" to="welcome0" />
+            <AnimeLink className="nav-list" to="welcome1" />
+            <AnimeLink className="nav-list" to="welcome2" />
+            <AnimeLink className="nav-list" to="welcome3" />
+          </div>
+          */}
+
+          <Element className="carousel-page carousel-page-0" id="welcome0">
+            <p>0000</p>
+          </Element>
+          <Element className="carousel-page carousel-page-1" id="welcome1">
+            <p>0001</p>
+          </Element>
+          <Element className="carousel-page carousel-page-2" id="welcome2">
+            <p>0002</p>
+          </Element>
+          <Element className="carousel-page carousel-page-3" id="welcome3">
+            <p>0003</p>
+          </Element>
+        </div>
+      </div>
+    );
+  }
+}
